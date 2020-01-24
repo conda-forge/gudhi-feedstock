82ef26fb3a62724843f8521ebafad968a08932cf
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2020-01-24 17:22:56 +0100

    Compile only Python module and install header library - not utils

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,27 +19,9 @@ cd version
 
 mkdir build && cd build
 
-:: Rollback to have utilities - begin
-:: cmake -LAH -G"NMake Makefiles" ^
-::   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
-::   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-::   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-::   -DPython_ADDITIONAL_VERSIONS=${PY_VER} ^
-::   -DPYTHON_EXECUTABLE="%PYTHON%" ^
-::   -DWITH_GUDHI_PYTHON=OFF ^
-::   ..
-:: if errorlevel 1 exit 1
-:: 
-:: cmake --build . --config %CMAKE_CONFIG% --target INSTALL
-:: if errorlevel 1 exit 1
-:: 
-:: cmake -DWITH_GUDHI_PYTHON=ON .
-:: if errorlevel 1 exit 1
-:: 
-:: cd python
-:: python setup.py install
-:: if errorlevel 1 exit 1
-:: Rollback to have utilities - end
+:: Lines to be removed :
+::  -DWITH_GUDHI_TEST=OFF ^
+::  -DWITH_GUDHI_UTILITIES=OFF ^
 
 cmake -LAH -G"NMake Makefiles" ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
@@ -47,7 +29,16 @@ cmake -LAH -G"NMake Makefiles" ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DPython_ADDITIONAL_VERSIONS=${PY_VER} ^
   -DPYTHON_EXECUTABLE="%PYTHON%" ^
-  -DWITH_GUDHI_PYTHON=ON ..
+  -DWITH_GUDHI_PYTHON=OFF ^
+  -DWITH_GUDHI_TEST=OFF ^
+  -DWITH_GUDHI_UTILITIES=OFF ^
+  ..
+if errorlevel 1 exit 1
+
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+if errorlevel 1 exit 1
+
+cmake -DWITH_GUDHI_PYTHON=ON .
 if errorlevel 1 exit 1
 
 cd python
