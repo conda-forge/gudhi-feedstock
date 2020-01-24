ce081ff97a1f9efc64425441000edaf2e3dbebaf
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2020-01-24 22:10:44 +0100

    Add some traces

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,16 @@
-set CMAKE_CONFIG="Release"
+set CMAKE_CONFIG=Release
 set CMAKE_GENERATOR=NMake Makefiles
 
 :: Construct user version from devel version
 
 mkdir build && cd build
 
-cmake -LAH -G"%CMAKE_GENERATOR%" ^
+echo cmake -LAH -G"%CMAKE_GENERATOR%" ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
-  -DUSER_VERSION_DIR=version ^
-  .. || goto :eof
+  -DUSER_VERSION_DIR=version ..
+cmake -LAH -G"%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" -DUSER_VERSION_DIR=version .. || goto :eof
 
+echo cmake --build . --config %CMAKE_CONFIG% --target USER_VERSION
 cmake --build . --config %CMAKE_CONFIG% --target USER_VERSION || goto :eof
 
 cd version
@@ -22,6 +23,16 @@ mkdir build && cd build
 ::  -DWITH_GUDHI_TEST=OFF ^
 ::  -DWITH_GUDHI_UTILITIES=OFF ^
 
+echo cmake -LAH -G"%CMAKE_GENERATOR%" ^
+  -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+  -DPython_ADDITIONAL_VERSIONS=${PY_VER} ^
+  -DPYTHON_EXECUTABLE="%PYTHON%" ^
+  -DWITH_GUDHI_PYTHON=OFF ^
+  -DWITH_GUDHI_TEST=OFF ^
+  -DWITH_GUDHI_UTILITIES=OFF ^
+  ..
 cmake -LAH -G"%CMAKE_GENERATOR%" ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
@@ -33,9 +44,12 @@ cmake -LAH -G"%CMAKE_GENERATOR%" ^
   -DWITH_GUDHI_UTILITIES=OFF ^
   .. || goto :eof
 
+echo cmake --build . --config %CMAKE_CONFIG% --target INSTALL
 cmake --build . --config %CMAKE_CONFIG% --target INSTALL || goto :eof
 
+echo cmake -DWITH_GUDHI_PYTHON=ON .
 cmake -DWITH_GUDHI_PYTHON=ON . || goto :eof
 
 cd python
+echo python setup.py install
 python setup.py install
