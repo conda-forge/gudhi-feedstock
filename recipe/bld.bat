20f0561cf18bb4e6e2404848d98370ef5475a1ad
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2020-01-24 16:03:21 +0100

    Remove utils for Windows as it fails

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,23 +19,37 @@ cd version
 
 mkdir build && cd build
 
+:: Rollback to have utilities - begin
+:: cmake -LAH -G"NMake Makefiles" ^
+::   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
+::   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+::   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+::   -DPython_ADDITIONAL_VERSIONS=${PY_VER} ^
+::   -DPYTHON_EXECUTABLE="%PYTHON%" ^
+::   -DWITH_GUDHI_PYTHON=OFF ^
+::   ..
+:: if errorlevel 1 exit 1
+:: 
+:: cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+:: if errorlevel 1 exit 1
+:: 
+:: cmake -DWITH_GUDHI_PYTHON=ON .
+:: if errorlevel 1 exit 1
+:: 
+:: cd python
+:: python setup.py install
+:: if errorlevel 1 exit 1
+:: Rollback to have utilities - end
+
 cmake -LAH -G"NMake Makefiles" ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DPython_ADDITIONAL_VERSIONS=${PY_VER} ^
   -DPYTHON_EXECUTABLE="%PYTHON%" ^
-  -DWITH_GUDHI_PYTHON=OFF ^
-  ..
-if errorlevel 1 exit 1
-
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
-if errorlevel 1 exit 1
-
-cmake -DWITH_GUDHI_PYTHON=ON .
+  -DWITH_GUDHI_PYTHON=ON .
 if errorlevel 1 exit 1
 
 cd python
 python setup.py install
 if errorlevel 1 exit 1
-
