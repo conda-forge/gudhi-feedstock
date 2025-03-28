f4ddd334e3623f60e5e992cad2f4ea19a3bdff74
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2025-03-28 09:48:08 +0100

    Some debug infos

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,39 +1,11 @@
 #!/bin/sh
 
-# Download submodule
-git submodule update --init
+conda info
 
-# Construct user version from devel version
-mkdir build && cd build
+conda list
 
-cmake \
-  -DWITH_GUDHI_THIRD_PARTY=OFF \
-  -DUSER_VERSION_DIR=version \
-  ..
+echo CXXFLAGS
+echo $CXXFLAGS
 
-make user_version
-
-cd version
-
-# Build and install user version
-
-mkdir build && cd build
-
-cmake ${CMAKE_ARGS} -LAH -G"$CMAKE_GENERATOR" \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_PREFIX_PATH=${PREFIX} \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DPython_EXECUTABLE="$PYTHON" \
-  -DWITH_GUDHI_PYTHON=OFF \
-  -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" \
-  -DCMAKE_C_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" \
-  ..
-
-# install include files and utils
-make install -j${CPU_COUNT}
-
-# install the python package
-cmake -DWITH_GUDHI_PYTHON=ON .
-cd python
-CXXFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" CFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" $PYTHON setup.py build_ext -j${CPU_COUNT}
-$PYTHON -m pip install . -vv
+echo CFLAGS
+echo $CFLAGS
